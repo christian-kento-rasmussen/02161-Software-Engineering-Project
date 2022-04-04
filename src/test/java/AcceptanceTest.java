7fce8064f40a8cc9893f4fbe19bff899ce010fc8
Rawstone <dr.rawstone@outlook.com>
2022-04-04 16:11:11 +0200

    Created AcceptanceTest
    AcceptanceTest allows us to run all tests in one go

---
+++ src/test/java/AcceptanceTest.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features",
+	plugin = { "html:target/cucumber/wikipedia.html"}, 
+	monochrome=true, 
+	snippets = SnippetType.CAMELCASE,
+	glue = { "dtu.projectmanagement"}
+	)
+public class AcceptanceTest {
+
+}
