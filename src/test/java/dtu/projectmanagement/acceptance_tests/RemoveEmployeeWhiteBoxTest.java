6fa674c8f8ecbd830a611099382717bd581349ca
williamsteffens <william.f.f.steffens@gmail.com>
2022-05-07 09:42:16 +0200

    created removeEmployeeWhiteBoxTest

---
+++ src/test/java/dtu/projectmanagement/acceptance_tests/RemoveEmployeeWhiteBoxTest.java
@@ -0,0 +1,22 @@
+package dtu.projectmanagement.acceptance_tests;
+
+import dtu.projectmanagement.app.ManagementApp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RemoveEmployeeWhiteBoxTest {
+
+    ManagementApp managementApp;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("qwe")
+    void testInputDataSetA() {
+
+    }
+}
